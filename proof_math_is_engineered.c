09af47ea67afa40a3f6dedf9c682d28cdf86b759
Aleksey Vaneev <aleksey.vaneev@gmail.com>
2022-07-18 04:20:28 +0300

    A mind-blowing update. "Someone" was already smart even before Big Bang.

---
+++ proof_math_is_engineered.c
@@ -0,0 +1,53 @@
+/**
+ * License
+ *
+ * Copyright (c) 2022 Aleksey Vaneev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#define PH_HASH_COUNT 15
+static inline uint8_t prvhash_core1( uint8_t* const Seed,
+	uint8_t* const lcg, uint8_t* const Hash )
+{
+	*Hash ^= (uint8_t) ( *Seed ^ 0x1 );
+	*lcg ^= (uint8_t) ( *Seed ^ 0x0 );
+	const uint8_t out = (uint8_t) ( *lcg ^ *Seed );
+	*Seed ^= *Hash;
+	return( out );
+}
+int main()
+{
+	uint8_t Seed = 0, lcg = 0;
+	uint8_t Hash[ PH_HASH_COUNT ] = { 0 };
+	int HashPos = 0;
+	for( int l = 0; l < 512; l++ )
+	{
+		uint16_t r = 0;
+		for( int k = 0; k < 16; k++ )
+		{
+			r <<= 1;
+			r |= prvhash_core1( &Seed, &lcg, Hash + HashPos );
+			if( ++HashPos == PH_HASH_COUNT ) HashPos = 0;
+		}
+		printf( "%i\n", (int) r );
+	}
+}
